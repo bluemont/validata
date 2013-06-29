d57bd10c6fb6d4b25f5cd41e0e378f40ed587cd0
David James <david@bluemontlabs.com>
2013-06-29 09:47:48 -0400

    version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject validata "0.1.1-SNAPSHOT"
+(defproject validata "0.1.2-SNAPSHOT"
   :description "Simple data validation for Clojure."
   :url "https://github.com/bluemont/validata"
   :license {:name "Eclipse Public License"
